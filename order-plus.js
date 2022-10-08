0f6ee1405399004370e725ef698b503e2c1df8b9
sijemss <sijemss@gmail.com>
2022-10-08 19:50:53 +0700

    Update order-plus.js

--- order-plus.js
+++ order-plus.js
@@ -4,17 +4,6 @@
 
 ===================================================================================================
 
-    Mengubah / Menghapus file javascript (js) ini dapat mengakibatkan :
-
-    1. Kerusakan / error pada template
-    2. Menghilangkan fungsi update / perbaikan otomatis pada template
-    3. Tidak akan mendapatkan support / bantuan dari kreator template
-
-    Terima kasih sudah menggunakan karya saya. :)
-
-    Salam,
-    KangRian.NET
-
     Terakhir Diperbarui : 1/6/2022 @ 14:41
 
 ================================================================================================ */
